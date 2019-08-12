12c3acbf71af37beb0e2516b606b8460486101bd
Haig Talarian <haigtalarian@gmail.com>
2019-08-12 13:26:07 -0700

    Updates

---
+++ precache-manifest.a7a3c1323c4f1bdff0c9bd70b372e4e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e9a3464855cc65ba41b32721d647f4",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4b557c0808c27676f9e0",
+    "url": "/Clicky-Game/static/css/main.5e457cdc.chunk.css"
+  },
+  {
+    "revision": "4dbc779420a3e2162ebe",
+    "url": "/Clicky-Game/static/js/2.9590dc6d.chunk.js"
+  },
+  {
+    "revision": "4b557c0808c27676f9e0",
+    "url": "/Clicky-Game/static/js/main.4d2b582b.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5db6ca733c4be2b6d9e6c2877a92ac8a.js"
+  "/Clicky-Game/precache-manifest.a7a3c1323c4f1bdff0c9bd70b372e4e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Clicky-Game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
